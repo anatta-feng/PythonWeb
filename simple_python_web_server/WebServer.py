a3ead93584a894a251c5f15ba9e304e4c39c8b5f
fxc0719 <1@fxcdev.com>
2017-12-17 23:59:19 +0800

    移除无用代码

--- simple_python_web_server/WebServer.py
+++ simple_python_web_server/WebServer.py
@@ -2,7 +2,6 @@
 from socket import *
 
 serverSocket = socket(AF_INET, SOCK_STREAM)  # Prepare a sever socket
-# Fill in start
 serverHost = '127.0.0.1'
 serverPort = 12000
 address = (serverHost, serverPort)
@@ -10,7 +9,6 @@ address = (serverHost, serverPort)
 serverSocket.bind(address)
 # 后 listen 等待
 serverSocket.listen(1)
-# Fill in end
 while True:
     # Establish the connection
     print('Ready to serve...')
@@ -18,27 +16,19 @@ while True:
     connectionSocket, addr = serverSocket.accept()  # tcp 握手
     try:
         # 接收
-        message = connectionSocket.recv(1024)  # Fill in start #Fill in end
+        message = connectionSocket.recv(1024)
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = f.readlines()  # Fill in start #Fill in end
+        outputdata = f.readlines()
         # Send one HTTP header line into socket
-        # Fill in start
         connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
-        # Fill in end
         # Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
         connectionSocket.close()
-        print('success')
     except IOError:
         # Send response message for file not found
-        # Fill in start
-        print('404 error')
         connectionSocket.send('HTTP/1.1 404 not found\r\n\r\n'.encode())
-        # Fill in end
         # Close client socket
-        # Fill in start
         connectionSocket.close()
-        # Fill in end
 serverSocket.close()
