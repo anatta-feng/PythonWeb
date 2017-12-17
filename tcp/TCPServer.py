9eedcf8d5e20b47236c9a90464b5427dca8a65ae
fxc0719 <1@fxcdev.com>
2017-12-17 23:31:04 +0800

    添加注释

--- tcp/TCPServer.py
+++ tcp/TCPServer.py
@@ -3,7 +3,7 @@ from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
+serverSocket.listen(1)  # 请求链接的最大数
 print('The server is ready to receive')
 while 1:
     connectionSocket, addr = serverSocket.accept()
