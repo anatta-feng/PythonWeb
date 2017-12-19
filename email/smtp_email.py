f23ac8e040d2958015c367080f7bc9e4f9b7b90a
fxc0719 <1@fxcdev.com>
2017-12-19 10:38:06 +0800

    complete send mail by smtplib

---
+++ email/smtp_email.py
@@ -0,0 +1,21 @@
+import smtplib # 负责发送邮件
+from email.mime.text import MIMEText # 负责构造邮件
+
+# 参数
+# 正文，MIME的subtype，文本编码格式
+msg = MIMEText('hello, send by python...', 'plain', 'utf-8')
+
+# 通过smtp发送
+# 输入Email地址和口令
+from_addr = input('From: ')
+password = input('Password: ')
+# 输入收件人地址
+to_addr = input('To: ')
+# 输入SMTP服务器地址
+smtp_server = input('SMTP server: ')
+
+server = smtplib.SMTP(smtp_server, 25)
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
